793040deb3743b0c28cf46ce31c7474906c71b92
nangudeprashant <30728347+nangudeprashant@users.noreply.github.com>
2018-01-05 13:56:27 +0530

    My first commit.

--- spring-5-mvc/src/main/java/com/baeldung/Spring5Application.java
+++ spring-5-mvc/src/main/java/com/baeldung/Spring5Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Spring5Application {
 
     public static void main(String[] args) {
+    	System.out.println("Hi All!!!!!!!!!!!!!!!!");
         SpringApplication.run(Spring5Application.class, args);
     }
 
